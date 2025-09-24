85b807054069716eee16c10c027daae53d18c928
ELANGO <elangothangarasan@gmail.com>
2025-09-24 12:53:35 -0500

    product insight page

---
+++ frontend/Next-js/pulse-next-app/src/app/product-insight/page.jsx
@@ -0,0 +1,27 @@
+"use client"
+import {useState} from "react";
+
+export default function ProductInsightPage(){
+
+    const [productName, setProductName] = useState("");
+    const [productDescription, setProductDescription] = useState("");
+    const [productLaunchDate, setProductLaunchDate] = useState("");
+ 
+    return(
+     <>
+       <div>
+           <div>
+               <form>
+                 <label>product Name</label>
+                 <input type="text" placeholder="enter your product name" onChange={(e) => {setProductName(e.target.value)}}/>
+                 <label>product description</label>
+                 <textarea placeholder="Enter product description" onChange={(e) =>{setProductDescription(e.target.value)}}></textarea>
+                 <lable>product launch date</lable>
+                 <input type="date" onChange={(e)=>{setProductLaunchDate(e.target.value)}}/>
+                 <button> search the product insights</button>
+               </form>               
+           </div>
+       </div>
+     </>
+    )
+}
